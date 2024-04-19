2a08403a678ee81b497e24ed67a67ca651807cb7
EJ Leskauskas <leskaus1@msu.edu>
2024-04-19 16:16:32 -0400

    chnages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+function MagicButton(){
+  return(
+    <button>Magic</button>
+  );
+}
+
 function App() {
   return (
     <div className="App">
