40acee1b3aa7a163ada52083d164873033a9446d
EJ Leskauskas <leskaus1@msu.edu>
2024-04-19 17:13:31 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function MagicButton() {
-  return(
-    <div> 
-      <h3>This is a magic button</h3>
-      <h2> Lets keep going!</h2>
-      <button>Magic</button>
-    </div>
-  );
-}
+fetch("https://api.balldontlie.io/v1/teams").then()
 
 function App() {
   return (
     <div className="App">
-      <header className="App-Header">
-        <MagicButton/>
-      </header>
+      <button> Generate NBA Team</button>
     </div>
   );
 }
