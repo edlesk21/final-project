4a4d27ccfd6b0d11018065b92055068dc3bacad5
EJ Leskauskas <leskaus1@msu.edu>
2024-04-21 14:02:36 -0400

    chnages

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,17 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js@2.7.1'
 const supabaseUrl = 'https://wjnqkzmmtlctjpkdwldo.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqbnFrem1tdGxjdGpwa2R3bGRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM1NjA1NTAsImV4cCI6MjAyOTEzNjU1MH0.0woPXQQGqa3IY223dbzaP-sp6HK8p25ttObTcbUUnJc'
 const supabase = createClient(supabaseUrl, supabaseKey)
+
+async function getTeams(){
+    let { data: teams, error } = await supabase
+    .from('teams')
+    .select('*')
+    
+    for (let team of teams) {
+        let teamList = document.getElementById('teams');
+        teamList.innerHTML += `<li>${team.full_name} - ${team.confrence} - ${team.division}</li>`;
+      }
+}
+
+getTeams();
